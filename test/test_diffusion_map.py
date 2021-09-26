d87beaea542395eae3b592246036d82cd218dea0
lepodl <luckyzlb15@163.com>
2021-09-26 15:38:19 +0800

    temporary, begin to add velocity field

--- test/test_diffusion_map.py
+++ test/test_diffusion_map.py
@@ -6,12 +6,16 @@
 import os
 import numpy as np
 import h5py
-import logging
 from sklearn.metrics import pairwise_distances
 from tools import diffusion_map
+# from helpers.plotfuncs import create_fig
+from helpers.plotcolors import myblue
+import matplotlib.pyplot as plt
 
-logging.basicConfig(level=logging.DEBUG)
-data_dir = os.getcwd() + "/../data"
+plt.style.use(['science', 'no-latex'])
+
+project_path = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0] + "/../"
+data_dir = os.path.join(project_path, "data/raw_data")
 
 
 def load_if_exist(func, *args, **kwargs):
@@ -25,10 +29,10 @@ def load_if_exist(func, *args, **kwargs):
 
 
 def read_fmri(mode="task_bold"):
-    data_path = os.path.join(data_dir, "DTI_voxel_network_mat_0719.mat")
+    data_path = os.path.join(data_dir, "DTI_voxel_network_mat_zenglongbin_new.mat")
     data = h5py.File(data_path, "r")
     if mode == "task_bold":
-        fmri = data["dti_evaluation"][:]
+        fmri = data["dti_high_low_ts"][:]
     elif mode == "rest_bold":
         fmri = data["dti_rest_state"][:]
     else:
@@ -37,40 +41,61 @@ def read_fmri(mode="task_bold"):
 
 
 def process_aff():
+    write_path = os.path.join(data_dir, "../processed_data")
+    os.makedirs(write_path, exist_ok=True)
+    fig, axes = plt.subplots(1, 3, figsize=(9, 6))
+    axes = axes.flatten()
     task_fmri = load_if_exist(read_fmri, data_dir, "task_fmri", mode="task_bold")
     voxels, T = task_fmri.shape
     print(f"task_fmri: voxels {voxels}, time_length {T}")
-    dcon = np.tanh(task_fmri)
-    print("N", dcon.shape[0])
-    perc = np.array([np.percentile(x, 90) for x in dcon])
-    for i in range(dcon.shape[0]):
-        dcon[i, dcon[i, :] < perc[i]] = 0
-    print("Minimum value is %f" % dcon.min())
-    neg_values = np.array([sum(dcon[i, :] < 0) for i in range(voxels)])
+    fc = np.corrcoef(task_fmri)
+    print(f"fc.shape: {fc.shape}")
+    fc_data = fc.flatten()
+    weights = np.ones_like(fc_data) / float(len(fc_data))
+    axes[0].hist(fc_data, weights=weights, density=True, color=myblue, bins=100)
+    axes[0].set_xlabel('corrcef')
+    axes[0].set_ylabel('probablity')
+    axes[0].set_title('original fc')
+
+    perc = np.array([np.percentile(x, 90) for x in fc])
+    for i in range(fc.shape[0]):
+        fc[i, fc[i, :] < perc[i]] = 0
+    print("Minimum value of processed fc is %f" % fc.min())
+    neg_values = np.array([sum(fc[i, :] < 0) for i in range(voxels)])
     print("Negative values occur in %d rows" % sum(neg_values > 0))
-    dcon[dcon < 0] = 0
-    aff = 1 - pairwise_distances(dcon, metric='cosine')
-    write_path = os.path.join(data_dir, "processed_data")
-    os.makedirs(write_path, exist_ok=True)
-    np.save(os.path.join(write_path, "affinity.npy"), aff)
+    fc[fc < 0] = 0
+    fc_data = fc.flatten()
+
+    axes[1].hist(fc_data, weights=weights, density=True, color=myblue)
+    # axes[1].set_xlim((-1., 1.))
+    axes[1].set_xlabel('corrcef')
+    axes[1].set_ylabel('probablity')
+    axes[1].set_title('processed fc')
+    del fc_data
+
+    aff = np.load(os.path.join(write_path, "affinity.npy"))
+    # aff = 1 - pairwise_distances(fc, metric='cosine')
+    aff_data = aff.flatten()
+    weights = np.ones_like(aff_data) / float(len(aff_data))
+    axes[2].hist(aff_data, weights=weights, density=True, color=myblue)
+    axes[2].set_xlabel('affinity')
+    axes[2].set_ylabel('probablity')
+    axes[2].set_title('affinity')
+    # np.save(os.path.join(write_path, "affinity.npy"), aff)
+    fig.savefig(os.path.join(write_path, "preprocessed_data.png"), dpi=300)
     print("Done!!!")
 
 
 def first_gradient_compotment():
-    log = logging.getLogger('test_first_gradient_compotment')
     aff = np.load(os.path.join(data_dir, "processed_data", "affinity.npy"))
     emb, res = diffusion_map.compute_diffusion_map(aff, alpha=0.5)
     np.save(os.path.join(data_dir, "processed_data", "emb.npy"), emb)
     np.save(os.path.join(data_dir, "processed_data", "res.npy"), res)
     a = [res['vectors'][:, i] / res['vectors'][:, 0] for i in range(100)]
     emb = np.array(a)[1:, :].T
-    log.info(f"len(emb):{len(emb)}")
-    log.info("Done")
+    print(f"len(emb):{len(emb)}")
+    print("Done")
 
 
-
-
-
-
-
-
+if __name__ == '__main__':
+    process_aff()
